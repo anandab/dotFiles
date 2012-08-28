c8bd0f2af1cd1e5dbfe559c19dc89b19d0227c19
rhacker <nxqd.inbox+rhacker@gmail.com>
2012-08-28 12:01:33 +0200

    1. Change irssi config to ignore all messages\n2.Add mutt

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,4 +1,7 @@
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/bin/core_perl:/usr/share/pear
+#your project folder
+export DEV=~/dev
+
 #rvm
 PATH=$HOME/.rvm/bin:$PATH
-PATH=$HOM/.dotfiles/bin:$PATH
+PATH=$HOME/.dotfiles/bin:$PATH
