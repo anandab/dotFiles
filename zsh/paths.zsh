3911635c8f1c9e1911df8c2c1189b91f28fbcadb
Quang Dung <nxqd.inbox@gmail.com>
2012-09-27 02:14:17 +0200

    done done done

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,7 +1,4 @@
-export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/bin/core_perl:/usr/share/pear
-#your project folder
-export DEV=~/Documents/dev
-
 #rvm
-PATH=$HOME/.rvm/bin:$PATH
+PATH=/usr/local/bin:$PATH
 PATH=$HOME/.dotfiles/bin:$PATH
+export DEV=~/Documents/dev
