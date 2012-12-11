b6454011a62322c073d608e387956cde2270fb13
Quang Dung <nxqd.inbox@gmail.com>
2012-12-11 20:14:16 +0100

    add path for z tool and npm

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,5 +1,8 @@
-#rvm
+#npm installed by homebrew
+PATH=/usr/local/share/npm/bin/:$PATH
 PATH=/usr/local/bin:$PATH
 PATH=$HOME/.dotfiles/bin:$PATH
 
 . `brew --prefix`/etc/profile.d/z.sh
+
+#npm installed by homebrew
