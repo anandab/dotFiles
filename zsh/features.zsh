02390269eb77d911f3f0f24db92f732c43b9275d
nXqd <nxqd.inbox@gmail.com>
2013-03-06 01:13:09 +0100

    daily

---
+++ zsh/features.zsh
@@ -0,0 +1,3 @@
+# up and down to cycle around history search
+bindkey '\e[A' history-search-backward
+bindkey '\e[B' history-search-forward
--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,9 +1,10 @@
 #npm installed by homebrew
-PATH=/usr/local/Cellar/php54/5.4.10/bin/:$PATH
+PATH=$(brew --prefix josegonzalez/php/php54)/bin:$PATH
 PATH=/usr/local/share/npm/bin/:$PATH
 PATH=/usr/local/bin:$PATH
-PATH=/usr/local/Cellar/ruby/1.9.3-p327/bin:$PATH
+PATH=/usr/local/opt/ruby/bin:$PATH
+PATH=/usr/local/share/python:$PATH
 PATH=$HOME/.dotfiles/bin:$PATH
+
 #rbenv path for hombrew
 export RBENV_ROOT=/usr/local/opt/rbenv
-eval "$(fasd --init auto)"
