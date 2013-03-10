416b86288a14db1c84ededfb8413a666a8e7b2b4
nXqd <nxqd.inbox@gmail.com>
2013-03-10 01:46:55 +0100

    move to bootstrap

---
+++ bin/bootstrap.sh
@@ -0,0 +1,67 @@
+#!/bin/zsh
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+echo "Don’t forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH."
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+
+# Install Bash 4
+brew install zsh
+
+# Install wget with IRI support
+brew install wget --enable-iri
+
+# Install more recent versions of some OS X tools
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
+brew tap josegonzalez/homebrew-php
+
+# Install everything else
+brew install mecurial
+brew install ack
+brew install fasd
+brew install git
+brew install rename
+brew install tree
+brew install vim
+brew install tmux
+brew install rtorrent
+brew install irssi
+brew install unrar
+brew install node
+brew install reattach-to-user-namespace
+brew install task
+brew install ctags
+brew install imagemagick
+
+# system monitor
+brew install htop
+
+# Developments
+brew install postgres --no-python
+brew install rbenv
+brew install ruby-build
+brew install rbenv-gem-rehash
+
+# rails critical gems
+gem install bundler foreman pg rails thin --no-rdoc --no-ri
+# heroku
+brew install heroku-toolbelt
+
+# fun !!, no fun no work
+gem install cloudapp_api
+gem install gifme
+
+# lint
+npm install jshint
+npm install csshint
+
+# Remove outdated versions from the cellar
+brew cleanup
